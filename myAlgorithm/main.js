682fc80a023f9d45b013fd0a8b4e03a8b3c84cd3
杨国龙 <yangguolong@yazuo.com>
2019-02-19 23:23:00 +0800

    插入排序

--- myAlgorithm/main.js
+++ myAlgorithm/main.js
@@ -1,1 +1,16 @@
-sadas
+let ary = [5,2,4,6,1,3]
+function insterSort(a) {
+    for(let j=1;j<a.length; j++){
+        key = a[j];
+        let i = j-1;
+        while(i>=0&&a[i]<key) {
+            let c = a[i];
+            a[i] = a[i+1];
+            a[i+1] = c;
+            i--;    
+        }
+    }
+    console.log(a)
+}
+insterSort(ary);
+console.log(0b11)
